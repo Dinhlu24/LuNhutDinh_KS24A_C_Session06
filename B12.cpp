355f788dfc28212b62f849d491959737e5e142b6
Dinhlu24 <dinhlunhut@gmail.com>
2024-12-17 14:38:27 +0700

    Update B12.cpp

--- B12.cpp
+++ B12.cpp
@@ -2,26 +2,14 @@
 
 int main(){
 	int n;	printf("Moi ban nhap vao so nguyen N: ");	scanf("%d", &n);
-	long long f1=1,f2=1,fn=f1+f2;
+	long long f0=0,f1=1,fn=0;
 	if(n > 0){
 		printf("Day la %d so Fibo: ",n);
-		if(n == 1){
-			printf("%lld",f1);
-			n=0;
-		}
-		else if(n == 2){
-			printf("%lld %lld",f1,f2);
-			n=0;
-		}
-		else{
-			n-=2;
-			printf("%lld %lld ",f1,f2);
-			while(n--){
-				printf("%lld ",fn);
-				f1=f2;
-				f2=fn;
-				fn=f1+f2;
-			}
+		while(n--){
+			printf("%lld ",fn);
+			f0=f1;
+			f1=fn;
+			fn=f0+f1;
 		}
 	}
 	else	printf("So ban nhap vao khong hop le");
