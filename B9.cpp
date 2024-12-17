d113845b6b3f9d6e4cf5445ec45da459b4009978
Dinhlu24 <dinhlunhut@gmail.com>
2024-12-17 13:20:40 +0700

    Update B9.cpp

--- B9.cpp
+++ B9.cpp
@@ -1,17 +1,10 @@
 #include <stdio.h>
-int is_Armstrong(int n){
-	int tmp = n,sum=0;
-	while(n){
-		int x = n%10;
-		sum += x*x*x;
-		n/=10;
-	}
-	return (sum == tmp);
-}
+#include <math.h>
 
 int main(){
-	printf("Sau day la so Armstrong hang tram: ");
+	printf("Tat ca so Armstrong hang tram: ");
 	for(int i=100;i<1000;i++){
-		if(is_Armstrong(i))	printf("%d ",i);
+		int sum = pow((i/100),3) + pow((i/10)%10,3) + pow(i%10,3);
+		if(sum == i)	printf("%d ",sum);
 	}
 }
